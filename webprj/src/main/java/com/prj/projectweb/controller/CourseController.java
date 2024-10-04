75924cd18b2211e7ef18cfa73901899eb655eb48
Lê Hà Phan <128871268+nhimtjofc@users.noreply.github.com>
2024-10-04 14:42:48 +0700

    Update CourseController.java

--- webprj/src/main/java/com/prj/projectweb/controller/CourseController.java
+++ webprj/src/main/java/com/prj/projectweb/controller/CourseController.java
@@ -63,4 +63,28 @@ public class CourseController {
                 .result(courseService.addGiangVienToCourse(course_id, giangVienRequest))
                 .build();
     }
+    @PutMapping("/edit/{course_id}")
+    public ApiResponse<String> editCourse(@PathVariable("course_id") Long courseId, @RequestBody CourseRequest courseRequest) {
+        log.info("in edit course controller");
+
+        try {
+            String message = courseService.editCourse(courseId, courseRequest);
+            return ApiResponse.<String>builder()
+                    .message(message)
+                    .result("Success")
+                    .build();
+        } catch (AppException e) {
+            // Trả về thông báo thất bại nếu xảy ra ngoại lệ
+            return ApiResponse.<String>builder()
+                    .message(e.getMessage())
+                    .result("Fail")
+                    .build();
+        } catch (Exception e) {
+            // Xử lý ngoại lệ chung
+            return ApiResponse.<String>builder()
+                    .message("Đã xảy ra lỗi: " + e.getMessage())
+                    .result("Fail")
+                    .build();
+        }
+    }
 }
