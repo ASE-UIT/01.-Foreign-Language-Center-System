3f03511bff689df9a121af96529a662543fdda09
Lê Hà Phan <128871268+nhimtjofc@users.noreply.github.com>
2024-10-08 07:49:09 +0700

    Update CourseRepository.java

--- webprj/src/main/java/com/prj/projectweb/repositories/CourseRepository.java
+++ webprj/src/main/java/com/prj/projectweb/repositories/CourseRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
     boolean existsByCourseName(String course);
+    @Query("SELECT c FROM Course c WHERE DATE(c.startTime) = :date")
+    List<Course> findClassesByDate(@Param("date") LocalDate date);
 }
