bd5bfc6232024515c3a96870e57b8601d4e80350
DoTraNgocChau <161134289+Hedgehog2411@users.noreply.github.com>
2024-10-09 23:06:26 +0700

    Delete webprj directory

--- webprj/src/main/java/com/prj/projectweb/repositories/PermissionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.prj.projectweb.repositories;
-
-import com.prj.projectweb.entities.Permission;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PermissionRepository extends JpaRepository<Permission, String> {
-}
