08cd593001a8856c1aee8be4602a3e28d59cc8ca
DoTraNgocChau <161134289+Hedgehog2411@users.noreply.github.com>
2024-10-09 22:16:09 +0700

    Delete 01.-Foreign-Language-Center-System-BackEnd directory

--- 01.-Foreign-Language-Center-System-BackEnd/webprj/src/main/java/com/prj/projectweb/repositories/CourseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.prj.projectweb.repositories;
-
-import com.prj.projectweb.entities.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-    boolean existsByCourseName(String course);
-}
