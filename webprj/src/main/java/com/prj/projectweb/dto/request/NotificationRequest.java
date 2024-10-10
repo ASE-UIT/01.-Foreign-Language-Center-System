58c9bafdec3bb9129467ab0bccfcab63e81e3346
Tran Thu <161853163+AnyaForger1201@users.noreply.github.com>
2024-10-10 23:32:57 +0700

    Delete webprj directory

--- webprj/src/main/java/com/prj/projectweb/dto/request/NotificationRequest.java
+++
@@ -1,8 +0,0 @@
-package com.prj.projectweb.dto.request;
-
-import lombok.Data;
-
-@Data
-public class NotificationRequest {
-    private Long userId; // ID của người dùng
-}
--- webprj/src/main/java/com/prj/projectweb/repositories/FileBoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.prj.projectweb.repositories;
-
-import com.prj.projectweb.entities.FileBoard;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileBoardRepository extends JpaRepository<FileBoard, Long> {
-}
