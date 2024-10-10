95841773c0f939bf7037a8ff26727517274c10fd
Tran Thu <161853163+AnyaForger1201@users.noreply.github.com>
2024-10-10 23:43:53 +0700

    Delete webprj directory

--- webprj/src/main/java/com/prj/projectweb/repositories/GiangVienRepository.java
+++
@@ -1,7 +0,0 @@
-package com.prj.projectweb.repositories;
-
-import com.prj.projectweb.entities.GiangVien;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GiangVienRepository extends JpaRepository<GiangVien, Long> {
-}
