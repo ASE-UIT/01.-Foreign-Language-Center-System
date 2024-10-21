d19ed299513cb8e47b36a23067ce649de4b09824
Lê Hà Phan <128871268+nhimtjofc@users.noreply.github.com>
2024-10-21 15:21:41 +0700

    Add files via upload

---
+++ webprj/src/main/java/com/prj/projectweb/dto/request/RefundRequest.java
@@ -0,0 +1,25 @@
+package com.prj.projectweb.dto.request;
+
+import com.prj.projectweb.exception.RefundAmount;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class RefundRequest {
+    // Getter và Setter
+    private String studentEmail;
+    private String courseName;
+    private String reason;
+    private RefundAmount refundAmount; // Enum chỉ định loại hoàn tiền
+
+    // Constructor
+    public RefundRequest(String studentEmail, String courseName, String reason, RefundAmount refundAmount) {
+        this.studentEmail = studentEmail;
+        this.courseName = courseName;
+        this.reason = reason;
+        this.refundAmount = refundAmount;
+    }
+
+}
+
